cce809d9353a89ae0c2c48b2efc22b2483a6a780
Ankur Seth <iankurseth@gmail.com>
2022-02-25 08:33:31 +0530

    Ignoring flags for now and printing only paths

--- src/tabbit.js
+++ src/tabbit.js
@@ -2,5 +2,33 @@
 
 'use strict';
 
-const paths = process.argv.slice(2);
-console.log(paths);
+const fs = require('fs');
+
+
+
+function Tabbit (paths, index) {
+	const path = paths[index];
+	const naturalIndex = parseInt(index) + 1;
+	console.log(`Tabbit (${naturalIndex}/${paths.length}): '${path}'`);
+}
+
+function main () {
+	const args = process.argv.slice(2);
+	const paths = [];
+
+	for (let arg of args) {
+		if (arg.startsWith("--")) {
+			// full flag
+		} else if (arg.startsWith("-")) {
+			// short flag
+		} else {
+			paths.push(arg);
+		}
+	}
+
+	for (let index in paths) {
+		Tabbit(paths, index);
+	}
+}
+
+main();
